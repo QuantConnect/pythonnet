3dfab861a0adfe81d44d2b4cd081984961696182
Benedikt Reinartz <filmor@gmail.com>
2019-06-20 17:59:28 +0200

    Revert "Enable C# parameters of type `object` accept any argument, passed from Python (#853)" (#882)
    This reverts commit 43c972d50fa2526f5f9ad73c591f6170c0538854.

--- src/runtime/converter.cs
+++ src/runtime/converter.cs
@@ -471,9 +471,12 @@ class GMT(tzinfo):
                     return ToArray(value, typeof(object[]), out result, setError);
                 }
 
-                Runtime.XIncref(value); // PyObject() assumes ownership
-                result = new PyObject(value);
-                return true;
+                if (setError)
+                {
+                    Exceptions.SetError(Exceptions.TypeError, "value cannot be converted to Object");
+                }
+
+                return false;
             }
 
             // Conversion to 'Type' is done using the same mappings as above for objects.
