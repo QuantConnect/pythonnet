030d3ce8f95acf06420e766de1d7ecfbf25c1b78
Colton Sellers <Colton.R.Sellers@gmail.com>
2021-07-13 21:02:24 +0000

    Cleanup V2

--- src/runtime/converter.cs
+++ src/runtime/converter.cs
@@ -457,7 +457,7 @@ class GMT(tzinfo):
                             catch
                             {
                                 // Failed to convert using implicit conversion,  must catch the error to stop program from exploding on Linux
-                                Exceptions.SetError(Exceptions.TypeError, $"Failed to implicitly convert {tmp} of {type} type to {obType} type");
+                                Exceptions.RaiseTypeError($"Failed to implicitly convert {type} to {obType}");
                                 return false;
                             }
                         }
@@ -643,7 +643,7 @@ class GMT(tzinfo):
                         catch
                         {
                             // Failed to convert using implicit conversion,  must catch the error to stop program from exploding on Linux
-                            Exceptions.SetError(Exceptions.TypeError, $"Failed to implicitly convert {result} of {result.GetType()} type to {obType} type");
+                            Exceptions.RaiseTypeError($"Failed to implicitly convert {result.GetType()} to {obType}");
                             return false;
                         }
                     }
